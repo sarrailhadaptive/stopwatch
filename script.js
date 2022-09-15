f626d74458a85536a28e1e9f118c9d7d76e9dd0c
Matias Sarrailh <matias.sarrailh@weareadaptive.com>
2022-09-15 11:26:11 +0200

    First Lap working in real time, laps after that also working good. Code is becoming a bit messy

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 let outputMinutes = document.getElementById('minutes');
 let outputSeconds = document.getElementById('seconds');
 let outputMiliSeconds = document.getElementById('miliSeconds');
-let lapNumber = 0; 
+let lapNumber = 2; 
 const startStopBtn = document.getElementsByClassName('startBtnBorder')[0];
 const resetLapBtn = document.getElementsByClassName('resetLapBtn')[0];
 const resetBtnBorder = document.getElementsByClassName('resetBtnBorder')[0];
 const startTimerBtn = document.getElementsByClassName('startTimerBtn')[0];
 const defaultLapList = document.getElementsByClassName('defaultLap');
+let lapMin;
+let lapSec;
+let lapMiliSec;
+let firstLapHTML = '';
 
 // DECLARING NECESSARY ELEMENTS
 let miliSeconds = 0;
@@ -21,8 +25,26 @@ let togglerLapReset = false;
 // STARTING EVENT LISTENERS
 startStopBtn.addEventListener('click', startTimer);
 
+// LOADERS
+function loadFirstLapHTML(){
+    lapMin = document.getElementsByClassName('lapMin')[0];
+    lapSec = document.getElementsByClassName('lapSec')[0];
+    lapMiliSec = document.getElementsByClassName('lapMiliSec')[0];
+    return lapMin, lapSec, lapMiliSec;
+}
+
 // STARTING AND STOPPING TIMER
 function startTimer(){
+    if(firstLapHTML === '') {
+        firstLapHTML = `
+            <p class="lap">Lap 1</p><p class="lapTime"><span class="lapMin"></span>:<span class="lapSec"></span>.<span class="lapMiliSec"></span></p>
+        `;
+        defaultLapList[0].insertAdjacentHTML('afterbegin', firstLapHTML);
+        loadFirstLapHTML();
+    }
+    lapMin.innerHTML = '00';
+    lapSec.innerHTML = '00';
+    lapMiliSec.innerHTML = '00';
     switchStartStopEventListeners();
     setStartStopBtnToStop();
     toggleLapResetBtn();
@@ -40,21 +62,35 @@ function stopTimer(){
 }
 
 function startCounting() {
+    loadFirstLapHTML();
     miliSeconds++;
-    if(miliSeconds <= 9) outputMiliSeconds.innerHTML = '0' + miliSeconds;
-    if(miliSeconds > 9) outputMiliSeconds.innerHTML = miliSeconds;
+    if(miliSeconds <= 9) {
+        outputMiliSeconds.innerHTML = '0' + miliSeconds;
+        lapMiliSec.innerHTML = '0' + miliSeconds;
+    }
+    if(miliSeconds > 9) {
+        outputMiliSeconds.innerHTML = miliSeconds;
+        lapMiliSec.innerHTML = miliSeconds;
+    }    
     if(miliSeconds > 59) {
         seconds++;
         outputSeconds.innerHTML = '0' + seconds;
+        lapSec.innerHTML = '0' + seconds;
         miliSeconds = 0;
         outputMiliSeconds.innerHTML = '0' + miliSeconds;
+        lapMiliSec.innerHTML = '0' + miliSeconds;
     };
-    if(seconds > 9) outputSeconds.innerHTML = seconds;
+    if(seconds > 9) {
+        outputSeconds.innerHTML = seconds;
+        lapSec.innerHTML = seconds;
+    }
     if(seconds > 59) {
         minutes++;
         outputMinutes.innerHTML = '0' + minutes;
+        lapMin.innerHTML = '0' + minutes;
         seconds = 0;
         outputSeconds.innerHTML = '0' + seconds;
+        lapSec.innerHTML = '0' + seconds;
     };
 }
 // ------------------------ //
@@ -134,7 +170,7 @@ function renderLap(){
         html += `
         <p class="lap">Lap ${lapNumber}</p><p class="lapTime">${lapTime[2]}:${lapTime[1]}.${lapTime[0]}</p>
         `;
-        defaultLapList[lapNumber].insertAdjacentHTML('afterbegin', html);
+        defaultLapList[lapNumber - 1].insertAdjacentHTML('afterbegin', html);
         console.log(lapNumber)
         return lapNumber++;
     };
